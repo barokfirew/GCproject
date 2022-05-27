d364ea964c1b9fb99ddba5fe6163787160d78df6
barok <firewkibreab@gmail.com>
2022-05-27 13:00:38 +0300

    Added my project

--- hotelmanage/urls.py
+++ hotelmanage/urls.py
@@ -24,6 +24,9 @@ from room.views import *
 from hotel.views import *
 from django.conf import settings
 from django.conf.urls.static import static
+
+from django.views.static import serve
+from django.conf.urls.static import url
 urlpatterns = [
     #path('jet/', include('jet.urls')),
     path('admin/', admin.site.urls),
@@ -84,4 +87,9 @@ urlpatterns = [
          deleteAnnouncement, name="deleteAnnouncement"),
     path('deleteBooking/<str:pk>/', deleteBooking, name="deleteBooking"),
     path('completeTask/<str:pk>/', completeTask, name="completeTask"),
+
+
+    url(r'^media/(?P<path>.*)$',  serve,{'document_root':       settings.MEDIA_ROOT}),
+    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
+
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
