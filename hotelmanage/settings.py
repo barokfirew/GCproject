a1996dbd1215b12304032e95b26d084e180abff7
barokfirew <84677853+barokfirew@users.noreply.github.com>
2022-05-27 19:46:52 +0300

    updated setting

--- hotelmanage/settings.py
+++ hotelmanage/settings.py
@@ -139,7 +139,10 @@ STATIC_URL = '/static/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'), ]
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'staticfiles'), 
+STATICFILES_STORAGE='whitenoise.django.GzipManifestStaticFilesStorage'                   
+                   
+]
 
 
 
