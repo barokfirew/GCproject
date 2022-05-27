e4eb7a36c580b51784cb90e7f7451b5caa35a4bd
barok <firewkibreab@gmail.com>
2022-05-27 16:53:21 +0300

    Added my projectt

--- hotelmanage/settings.py
+++ hotelmanage/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'on9*bf65je3#4+jphqufropk!s9*i&$*54@_9t8^6+c)iro2&q'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG =True
+DEBUG =False
 
 ALLOWED_HOSTS = []
 #'127.0.0.1', 'sampledomain.com'
@@ -54,7 +54,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    #'whitenoise.middleware.WhiteNoiseMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
--- hotelmanage/urls.py
+++ hotelmanage/urls.py
@@ -89,7 +89,7 @@ urlpatterns = [
     path('completeTask/<str:pk>/', completeTask, name="completeTask"),
 
 
-    #re_path(r'^media/(?P<path>.*)$',  serve, {'document_root':       settings.MEDIA_ROOT}),
-    #re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
+    re_path(r'^media/(?P<path>.*)$',  serve, {'document_root':       settings.MEDIA_ROOT}),
+    re_path(r'^static/(?P<path>.*)$', serve, {'document_root': settings.STATIC_ROOT}),
 
 ]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
