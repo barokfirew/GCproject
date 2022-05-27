0263473efee01e5eb7938cfded799e3840ca73a8
barokfirew <84677853+barokfirew@users.noreply.github.com>
2022-05-27 18:24:57 +0300

    Allowed Hosts

--- hotelmanage/settings.py
+++ hotelmanage/settings.py
@@ -26,8 +26,8 @@ SECRET_KEY = 'on9*bf65je3#4+jphqufropk!s9*i&$*54@_9t8^6+c)iro2&q'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG =False
 
-ALLOWED_HOSTS = []
-#'127.0.0.1', 'sampledomain.com'
+ALLOWED_HOSTS = ['127.0.0.1', 'sampledomain.com']
+
 
 # Application definition
 
